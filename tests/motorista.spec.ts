e7c05d62eea2af9b750ab1ff608f903bd25875b4
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2026-05-21 14:58:58 -0300

    Test motoristas

--- tests/motorista.spec.ts
+++ tests/motorista.spec.ts
@@ -1,17 +1,25 @@
 import { test, expect } from '@playwright/test';
-import { MotoristaPage } from '../pages/motorista-page';
-import { LoginPage } from '../pages/login-page';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
-
-const email = process.env.USER_EMAIL!;
-const password = process.env.USER_PASSWORD!;
-
-test('Usuário pode cadastrar um motorista', async ({ page }) => {
-    const loginpage = new LoginPage(page);
-    await loginpage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-
+test('test', async ({ page }) => {
+  await page.goto('https://omnibusdrive.up.railway.app/login');
+  await page.locator('input[name="email"]').click();
+  await page.locator('input[name="email"]').fill('jp@gmail.com');
+  await page.locator('input[name="password"]').click();
+  await page.locator('input[name="password"]').fill('12345678');
+  await page.getByRole('button', { name: 'ENTRAR' }).click();
+  await page.getByRole('button', { name: 'Motoristas' }).click();
+  await page.getByRole('button', { name: 'Cadastrar motorista' }).click();
+  await page.getByRole('textbox', { name: 'Ex: José Bonifácio Sombra' }).click();
+  await page.getByRole('textbox', { name: 'Ex: José Bonifácio Sombra' }).fill('Jose');
+  await page.getByRole('textbox', { name: 'Ex: jose@gmail.com' }).click();
+  await page.getByRole('textbox', { name: 'Ex: jose@gmail.com' }).fill('jose2@gmail.com');
+  await page.getByRole('textbox', { name: 'Ex: (88) 94002-' }).click();
+  await page.getByRole('textbox', { name: 'Ex: (88) 94002-' }).fill('(88) 88888-88888');
+  await page.getByRole('textbox', { name: 'Ex: 07234567889' }).click();
+  await page.getByRole('textbox', { name: 'Ex: 07234567889' }).fill('647283746282');
+  await page.getByRole('textbox', { name: 'Mínimo 8 caracteres' }).click();
+  await page.getByRole('textbox', { name: 'Mínimo 8 caracteres' }).fill('12345678');
+  await page.getByRole('textbox', { name: 'Digite a senha novamente' }).click();
+  await page.getByRole('textbox', { name: 'Digite a senha novamente' }).fill('12345678');
+  await page.getByRole('button', { name: 'Cadastrar' }).click();
 });
-
