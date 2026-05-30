700208eb7cb6184e6e2b75fbcee97bed75e7e7e8
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2026-05-30 17:48:34 -0300

    Adicionar testes para registro e exclusão de matérias na suíte de testes

---
+++ tests/subject.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+import { SubjectsPage } from '../pages/subjects-page';
+import { LoginPage } from '../pages/login-page';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const email = process.env.USER_EMAIL!;
+const password = process.env.USER_PASSWORD!;
+
+test('O usuário pode registrar uma nova matéria', async ({ page }) => {
+    
+    const loginPage = new LoginPage(page);
+    await loginPage.login(email, password);
+    await expect(page).toHaveURL(/.dashboard/);
+    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
+
+    const subjectsPage = new SubjectsPage(page);
+    await subjectsPage.cadastrarSubject(
+        'Matemática', 
+        'Prof. Silva', 
+        '1'
+    );
+    
+    await expect(page.getByText('Matéria cadastrada!')).toBeVisible();
+});
+
+test('O usuário pode excluir uma matéria existente', async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+    await loginPage.login(email, password);
+    await expect(page).toHaveURL(/.dashboard/);
+    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
+
+    const subjectsPage = new SubjectsPage(page);
+    await subjectsPage.excluirSubject('Matemática');
+
+    await expect(page.getByText('Matéria excluída!')).toBeVisible();
+});
+
