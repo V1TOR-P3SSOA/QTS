25082d6cd4dc42c9f93cd70fd5bc5083cdd10ed2
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2026-05-30 17:47:52 -0300

    Atualizar URL de login e corrigir seletores de entrada no LoginPage

--- pages/login-page.ts
+++ pages/login-page.ts
@@ -1,5 +1,5 @@
 import type { Page } from '@playwright/test';
-import { expect } from '@playwright/test';
+import { expect } from '@playwright/test';  
 
 export class LoginPage {
     readonly page: Page;
@@ -7,9 +7,9 @@ constructor(page: Page) {
         this.page = page;
     }
 async login(email: string, password: string) {
-        await this.page.goto('https://omnibusdrive.up.railway.app/login');
-        await this.page.type('input[name="email"]', email);
-        await this.page.type('input[name="password"]', password);
+        await this.page.goto('https://studylab.free.laravel.cloud/login');
+        await this.page.fill('input[id="email"]', email);
+        await this.page.fill('input[id="password"]', password);
         await this.page.click('button[type="submit"]');
     }
 
