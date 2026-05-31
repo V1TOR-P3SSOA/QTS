543c7d89ccc4cb4dc02150db01b8670b61276405
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2026-05-31 13:37:41 -0300

    refatorar testes de matérias, organizando casos felizes, tristes e de borda

--- tests/subject.spec.ts
+++ tests/subject.spec.ts
@@ -8,80 +8,59 @@ dotenv.config();
 const email = process.env.USER_EMAIL!;
 const password = process.env.USER_PASSWORD!;
 
-test('O usuário pode registrar uma nova matéria', async ({ page }) => {
-    
+test.beforeEach(async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.login(email, password);
     await expect(page).toHaveURL(/.dashboard/);
     await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
-
-    const subjectsPage = new SubjectsPage(page);
-    await subjectsPage.cadastrarSubject(
-        'Matemática', 
-        'Prof. Silva', 
-        '1'
-    );
-    
-    await expect(page.getByText('Matéria cadastrada!')).toBeVisible();
 });
 
-test('O usuário pode excluir uma matéria existente', async ({ page }) => {
+test.describe('Casos Felizes', () => {
 
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
+    test('O usuário pode registrar uma nova matéria', async ({ page }) => {
+        const subjectsPage = new SubjectsPage(page);
+        await subjectsPage.cadastrarSubject('Matemática', 'Prof. Silva', '1');
+        await expect(page.getByText('Matéria cadastrada!', { exact: false })).toBeVisible();
+    });
 
-    const subjectsPage = new SubjectsPage(page);
-    await subjectsPage.excluirSubject('Matemática');
+    test('O usuário pode excluir uma matéria existente', async ({ page }) => {
+        const subjectsPage = new SubjectsPage(page);
+        await subjectsPage.cadastrarSubject('Matemática', 'Prof. Silva', '1');
+        await subjectsPage.excluirSubject('Matemática');
+        await expect(page.getByText('Matéria excluída!', { exact: false })).toBeVisible();
+    });
 
-    await expect(page.getByText('Matéria excluída!')).toBeVisible();
 });
 
-test('O usuário não pode cadastrar uma matéria sem preencher o campo de professor', async ({ page }) => {
+test.describe('Casos Tristes', () => {
 
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
+    test('O usuário não pode cadastrar uma matéria sem preencher o campo de professor', async ({ page }) => {
+        const subjectsPage = new SubjectsPage(page);
+        await subjectsPage.cadastrarSubject('Matemática', '', '1');
+        await expect(page.getByText('Informe o nome do professor.', { exact: false })).toBeVisible();
+    });
 
-    const subjectsPage = new SubjectsPage(page);
-    await subjectsPage.cadastrarSubject('Matemática', '', '1');
-    await expect(page.getByText('Informe o nome do professor.')).toBeVisible();
+    test('O usuário não pode cadastrar uma matéria com um nome que já existe', async ({ page }) => {
+        const subjectsPage = new SubjectsPage(page);
+        await subjectsPage.cadastrarSubject('Química', 'Prof. Silva', '1');
+        await subjectsPage.cadastrarSubject('Química', 'Prof. Silva', '1');
+        await expect(page.getByText('Você já possui uma matéria com este nome.', { exact: false })).toBeVisible();
+    });
+
 });
 
-test('O usuário não pode cadastrar uma matéria com um nome de matéria que já existe', async ({ page }) => {
+test.describe('Casos de Borda', () => {
 
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
+    test('O usuário não pode registrar o nome do professor com números', async ({ page }) => {
+        const subjectsPage = new SubjectsPage(page);
+        await subjectsPage.cadastrarSubject('Física', 'Prof. Silva 123', '1');
+        await expect(page.getByText('O campo professor não pode conter números.', { exact: false })).toBeVisible();
+    });
 
-    const subjectsPage = new SubjectsPage(page);
-    await subjectsPage.cadastrarSubject('Química', 'Prof. Silva', '1');
-    await expect(page.getByText('Você já possui uma matéria com este nome.')).toBeVisible();
-});
+    test('O usuário não pode registrar uma matéria com semestre igual a 21', async ({ page }) => {
+        const subjectsPage = new SubjectsPage(page);
+        await subjectsPage.cadastrarSubject('Biologia', 'Prof. Silva', '21');
+        await expect(page.getByText('O semestre não pode ser maior que 20.', { exact: false })).toBeVisible();
+    });
 
-test('O usuário não pode resgistrar o nome do professor com caracteres inválidos', async ({ page }) => {
-
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
-
-    const subjectsPage = new SubjectsPage(page);
-    await subjectsPage.cadastrarSubject('Física', '12345678', '1');
-    await expect(page.getByText('O campo professor não pode conter números.')).toBeVisible();
-});
-
-test('O usuário não pode registrar uma matéria com um semestre inválido', async ({ page }) => {
-
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
-
-    const subjectsPage = new SubjectsPage(page);
-    await subjectsPage.cadastrarSubject('Biologia', 'Prof. Silva', '99');
-    await expect(page.getByText('O semestre não pode ser maior que 20.')).toBeVisible();
 });
