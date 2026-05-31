ac2c84893bda5d783c8fd173fb2715d491fd32cc
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2026-05-31 11:04:20 -0300

    remover parâmetro de tempo do método cadastrarExam e adicionar método editarExam

--- pages/exams-page.ts
+++ pages/exams-page.ts
@@ -14,30 +14,37 @@ constructor(page: Page) {
         await this.page.click('button[type="submit"]');
     }
 
-async cadastrarExam(prova: string, materia: string, tempo: string) {
+async cadastrarExam(prova: string, materia: string) {
     await this.page.goto('https://studylab.free.laravel.cloud/exams');
     await this.page.waitForLoadState('networkidle');
-    
-    
     await this.page.click('button[id="prevWeek"]');
-    
     await this.page.locator('button:has-text("Adicionar")').first().click();
     await this.page.locator('select[id="modalType"]').selectOption(prova);
     await this.page.locator('select[id="modalDesc"]').selectOption('__outro__');
     await this.page.fill('input[id="descCustom"]', materia);
-    await this.page.fill('input[id="modalTime"]', tempo);
     await this.page.click('button[id="modalSave"]');
 }
 
 async excluirExam(materia: string) {
     await this.page.goto('https://studylab.free.laravel.cloud/exams');
-        await this.page.waitForLoadState('networkidle');
+    await this.page.waitForLoadState('networkidle');
     await this.page.click('button[id="prevWeek"]');
     const card = this.page.locator('div[onclick*="openEdit"]').filter({ hasText: materia }).first();
     await card.click();
     await this.page.click('button[id="modalDelete"]');
     await expect(this.page.getByText('Excluir prova?', { exact: false })).toBeVisible();
     await this.page.click('button[id="confirmModalOk"]');
+}
+
+async editarExam(materiaAtual: string, materiaNova: string) {
+    await this.page.goto('https://studylab.free.laravel.cloud/exams');
+    await this.page.waitForLoadState('networkidle');
+    await this.page.click('button[id="prevWeek"]');
+    const card = this.page.locator('div[onclick*="openEdit"]').filter({ hasText: materiaAtual }).first();
+    await card.click();
+    await this.page.locator('select[id="modalDesc"]').selectOption('__outro__');
+    await this.page.fill('input[id="descCustom"]', materiaNova);
+    await this.page.click('button[id="modalSave"]');
 }
 
 }
