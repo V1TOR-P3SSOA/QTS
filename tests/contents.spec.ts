e419b39bcd63ff5f23a807737a269400e43ea459
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2026-05-31 13:37:46 -0300

    refatorar testes de conteúdo, organizando casos felizes, tristes e de borda

--- tests/contents.spec.ts
+++ tests/contents.spec.ts
@@ -8,80 +8,59 @@ dotenv.config();
 const email = process.env.USER_EMAIL!;
 const password = process.env.USER_PASSWORD!;
 
-test('O usuário pode registrar um novo conteúdo', async ({ page }) => {
-    
+test.beforeEach(async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.login(email, password);
     await expect(page).toHaveURL(/.dashboard/);
     await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
-
-    const contentPage = new ContentPage(page);
-    await contentPage.cadastrarContent(
-        'Hidrocabornetos', 
-        'Química', 
-        '20'
-    );
-    
-    await expect(page.getByText('Conteúdo cadastrado!')).toBeVisible();
 });
 
-test('O usuário pode excluir um conteúdo existente', async ({ page }) => {
+test.describe('Casos Felizes', () => {
 
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
+    test('O usuário pode registrar um novo conteúdo', async ({ page }) => {
+        const contentPage = new ContentPage(page);
+        await contentPage.cadastrarContent('Hidrocarbonetos', 'Química', '20');
+        await expect(page.getByText('Conteúdo cadastrado!', { exact: false })).toBeVisible();
+    });
 
-    const contentPage = new ContentPage(page);
-    await contentPage.excluirContent('Hidrocabornetos');
+    test('O usuário pode excluir um conteúdo existente', async ({ page }) => {
+        const contentPage = new ContentPage(page);
+        await contentPage.cadastrarContent('Hidrocarbonetos', 'Química', '20');
+        await contentPage.excluirContent('Hidrocarbonetos');
+        await expect(page.getByText('Conteúdo excluído!', { exact: false })).toBeVisible();
+    });
 
-    await expect(page.getByText('Conteúdo excluído!')).toBeVisible();
 });
 
-test('O usuário não pode cadastrar um conteúdo sem preencher o campo de nome', async ({ page }) => {
+test.describe('Casos Tristes', () => {
 
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
+    test('O usuário não pode cadastrar um conteúdo sem preencher o campo de nome', async ({ page }) => {
+        const contentPage = new ContentPage(page);
+        await contentPage.cadastrarContent('', 'Química', '20');
+        await expect(page.getByText('Informe o nome do conteúdo.', { exact: false })).toBeVisible();
+    });
 
-    const contentPage = new ContentPage(page);
-    await contentPage.cadastrarContent('', 'Química', '20');
-    await expect(page.getByText('Informe o nome do conteúdo.')).toBeVisible();
+    test('O usuário não pode cadastrar um conteúdo com um nome que já existe', async ({ page }) => {
+        const contentPage = new ContentPage(page);
+        await contentPage.cadastrarContent('Funções oxigenadas', 'Química', '20');
+        await contentPage.cadastrarContent('Funções oxigenadas', 'Química', '20');
+        await expect(page.getByText('Este conteúdo já está registrado.', { exact: false })).toBeVisible();
+    });
+
 });
 
-test('O usuário não pode cadastrar um conteúdo com um nome que já existe', async ({ page }) => {
+test.describe('Casos de Borda', () => {
 
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
+    test('O usuário não pode registrar o nome do conteúdo com números', async ({ page }) => {
+        const contentPage = new ContentPage(page);
+        await contentPage.cadastrarContent('Química 123', 'Química', '1');
+        await expect(page.getByText('O nome do conteúdo não pode conter números ou caracteres especiais.', { exact: false })).toBeVisible();
+    });
 
-    const contentPage = new ContentPage(page);
-    await contentPage.cadastrarContent('Funções oxigenadas', 'Química', '20');
-    await expect(page.getByText('Este conteúdo já está registrado.')).toBeVisible();
-});
+    test('O usuário não pode registrar um conteúdo com semestre igual a 21', async ({ page }) => {
+        const contentPage = new ContentPage(page);
+        await contentPage.cadastrarContent('Hidrocarbonetos', 'Química', '21');
+        await expect(page.getByText('O semestre não pode ser maior que 20.', { exact: false })).toBeVisible();
+    });
 
-test('O usuário não pode resgistrar o nome do conteúdo com caracteres inválidos', async ({ page }) => {
-
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
-
-    const contentPage = new ContentPage(page);
-    await contentPage.cadastrarContent('', '12345678', '1');
-    await expect(page.getByText('O campo nome não pode conter números.')).toBeVisible();
-});
-
-test('O usuário não pode registrar um conteúdo com um semestre inválido', async ({ page }) => {
-
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
-
-    const contentPage = new ContentPage(page);
-    await contentPage.cadastrarContent('Hidrocabornetos', 'Química', '99');
-    await expect(page.getByText('O semestre não pode ser maior que 20.')).toBeVisible();
 });
