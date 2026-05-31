e72546094d89d2ba7932d5cdc62bb95c3dea3ba3
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2026-05-31 11:04:27 -0300

    refatorar testes de cadastro e edição de provas, incluindo validações de campos obrigatórios

--- tests/exams.spec.ts
+++ tests/exams.spec.ts
@@ -8,27 +8,63 @@ dotenv.config();
 const email = process.env.USER_EMAIL!;
 const password = process.env.USER_PASSWORD!;
 
-test('O usuário pode registrar uma nova prova', async ({ page }) => {
-    
+test.beforeEach(async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.login(email, password);
     await expect(page).toHaveURL(/.dashboard/);
     await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
-
-    const examsPage = new ExamsPage(page);
-    await examsPage.cadastrarExam('Prova', 'Matemática', '11111212');
-    await expect(page.getByText('Prova cadastrada!', { exact: false })).toBeVisible();
 });
 
-test('O usuário pode excluir uma prova existente', async ({ page }) => {
+test.describe('Casos Felizes', () => {
 
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
+    test('O usuário pode registrar uma nova prova', async ({ page }) => {
+        const examsPage = new ExamsPage(page);
+        await examsPage.cadastrarExam('Prova', 'Matemática');
+        await expect(page.getByText('Prova cadastrada!', { exact: false })).toBeVisible();
+    });
 
+    test('O usuário pode excluir uma prova existente', async ({ page }) => {
+        const examsPage = new ExamsPage(page);
+        await examsPage.cadastrarExam('Prova', 'Matemática');
+        await examsPage.excluirExam('Matemática');
+        await expect(page.getByText('Prova excluída.', { exact: false })).toBeVisible();
+    });
+
+});
+
+test.describe('Casos Tristes', () => {
+
+    test('O usuário não pode cadastrar uma prova sem preencher o campo de matéria', async ({ page }) => {
+        const examsPage = new ExamsPage(page);
+        await examsPage.cadastrarExam('Prova', '');
+        await expect(page.getByText('Informe uma matéria sem caracteres especiais (máx 40).')).toBeVisible();
+    });
+
+    test('O usuário não pode editar uma prova sem preencher o campo de matéria', async ({ page }) => {
+        const examsPage = new ExamsPage(page);
+        await examsPage.cadastrarExam('Prova', 'Matemática');
+        await examsPage.editarExam('Matemática', '');
+        await expect(page.getByText('Informe uma matéria sem caracteres especiais (máx 40).')).toBeVisible();
+    });
+
+
+
+});
+
+test.describe('Casos de Borda', () => {
+
+    test('O usuário pode cadastrar uma prova com o nome da matéria no limite máximo de caracteres', async ({ page }) => {
+        const examsPage = new ExamsPage(page);
+        const longSubjectName = 'A'.repeat(40);
+        await examsPage.cadastrarExam('Prova', longSubjectName);
+        await expect(page.getByText('Prova cadastrada!', { exact: false })).toBeVisible();
+    });
+
+    test('O usuário pode editar uma prova para ter o nome da matéria no limite máximo de caracteres', async ({ page }) => {
     const examsPage = new ExamsPage(page);
-    await examsPage.excluirExam('Matemática');
-
-    await expect(page.getByText('Prova excluída.', { exact: false })).toBeVisible();
+    await examsPage.cadastrarExam('Prova', 'Matemática');
+    const longSubjectName = 'B'.repeat(40);
+    await examsPage.editarExam('Matemática', longSubjectName);
+    await expect(page.getByText('Prova atualizada!', { exact: false })).toBeVisible();
+});
 });
