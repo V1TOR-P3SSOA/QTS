66e98135520b3c34085db88e378e274b66f85e1a
Joa0-Pedr0 <nogueiralimajoaopedro@gmail.com>
2026-05-31 12:28:58 -0300

    refatorar testes de atividades, organizando casos de sucesso, falha e borda

--- tests/activities.spec.ts
+++ tests/activities.spec.ts
@@ -8,44 +8,83 @@ dotenv.config();
 const email = process.env.USER_EMAIL!;
 const password = process.env.USER_PASSWORD!;
 
-test('O usuário pode registrar uma nova atividade', async ({ page }) => {
-    
+test.beforeEach(async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.login(email, password);
     await expect(page).toHaveURL(/.dashboard/);
     await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
+});
 
+test.describe('Casos Felizes', () => {
+
+    test('O usuário pode registrar uma nova atividade', async ({ page }) => {
+        const activitiesPage = new ActivitiesPage(page);
+        await activitiesPage.cadastrarActivitie(
+            'Estudar para a prova de matemática',
+            'MAT',
+            'Hoje',
+        );
+        await expect(page.getByText('Estudar para a prova de matemática')).toBeVisible();
+    });
+
+    test('O usuário pode concluir uma atividade existente', async ({ page }) => {
+        const activitiesPage = new ActivitiesPage(page);
+        await activitiesPage.cadastrarActivitie(
+            'Estudar para a prova de matemática',
+            'MAT',
+            'Hoje',
+        );
+        await activitiesPage.excluirActivitie('Estudar para a prova de matemática');
+    });
+
+});
+
+test.describe('Casos Tristes', () => {
+
+    test('O usuário não pode cadastrar uma atividade sem preencher o campo de descrição', async ({ page }) => {
+        const activitiesPage = new ActivitiesPage(page);
+        await activitiesPage.cadastrarActivitie(
+            '',
+            'MAT',
+            'Hoje',
+        );
+        await expect(page.getByText('Informe a descrição.')).toBeVisible();
+    });
+
+    test('O usuário não pode cadastrar uma atividade sem preencher o campo data de vencimento', async ({ page }) => {
+        const activitiesPage = new ActivitiesPage(page);
+        await activitiesPage.cadastrarActivitie(
+            'Estudar para a prova de matemática',
+            'MAT',
+            '',
+        );
+        await expect(page.getByText('Informe a data de vencimento.')).toBeVisible();
+    });
+
+});
+
+test.describe('Casos de Borda', () => {
+
+    test('O usuário pode cadastrar uma atividade com a descrição no limite máximo de caracteres', async ({ page }) => {
+        
+        const activitiesPage = new ActivitiesPage(page);
+        const longDescription = 'A'.repeat(255);
+        await activitiesPage.cadastrarActivitie(
+            longDescription,
+            'MAT',
+            'Hoje',
+        );
+        await expect(page.getByText('Atividade cadastrada!')).toBeVisible();
+    });
+
+    test('O usuário não pode cadastrar uma atividade com data de vencimento no passado', async ({ page }) => {
     const activitiesPage = new ActivitiesPage(page);
-    await activitiesPage.cadastrarActivitie(
+    await activitiesPage.cadastrarActivitieComData(
         'Estudar para a prova de matemática',
         'MAT',
-        'Hoje',
+        '1111-11-11',
     );
+    await expect(page.getByText('A data não pode estar no passado.', { exact: false })).toBeVisible();
+    });
 
-    await expect(page.getByText('Estudar para a prova de matemática')).toBeVisible();
-});
-
-test('O usuário pode concluir uma atividade existente', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
-
-    const activitiesPage = new ActivitiesPage(page);
-    await activitiesPage.excluirActivitie('Estudar para a prova de matemática');
-});
-
-test('O usuário não pode cadastrar uma atividade sem preencher o campo de descrição', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.login(email, password);
-    await expect(page).toHaveURL(/.dashboard/);
-    await expect(page.getByText('Olá, Pessôa', { exact: false })).toBeVisible();
-
-    const activitiesPage = new ActivitiesPage(page);
-    await activitiesPage.cadastrarActivitie(
-        '',
-        'MAT',
-        'Hoje',
-    );
-    await expect(page.getByText('Informe a descrição.')).toBeVisible();
 });
